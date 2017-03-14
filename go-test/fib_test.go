8ad1859fa0bd34a773b3a8f6c29273c4a75dfec3
Jonathan André Schweder <jonathanschweder@gmail.com>
2017-03-14 20:53:02 -0300

    add go-test example

---
+++ go-test/fib_test.go
@@ -0,0 +1,39 @@
+package main
+
+import "testing"
+
+var fibTests = []struct {
+	n        int // input
+	expected int // expected result
+}{
+	{1, 1},
+	{2, 1},
+	{3, 2},
+	{4, 3},
+	{5, 5},
+	{6, 8},
+	{7, 13},
+}
+
+func TestFib(t *testing.T) {
+	for _, tt := range fibTests {
+		actual := Fib(tt.n)
+		if actual != tt.expected {
+			t.Errorf("Fib(%d): expected %d, actual %d", tt.n, tt.expected, actual)
+		}
+	}
+}
+
+func benchmarkFib(i int, b *testing.B) {
+    // run the Fib functions b.N times
+    for n := 0; n < b.N; n++ {
+        Fib(i)
+    }
+}
+
+func BenchmarkFib1(b *testing.B)  { benchmarkFib(1, b) }
+func BenchmarkFib2(b *testing.B)  { benchmarkFib(2, b) }
+func BenchmarkFib3(b *testing.B)  { benchmarkFib(3, b) }
+func BenchmarkFib10(b *testing.B) { benchmarkFib(10, b) }
+func BenchmarkFib20(b *testing.B) { benchmarkFib(20, b) }
+func BenchmarkFib40(b *testing.B) { benchmarkFib(40, b) }
