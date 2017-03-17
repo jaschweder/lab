749d948edb59a495eb276904e99beeccaee62925
Jonathan A. Schweder <jonathanschweder@gmail.com>
2017-03-17 13:33:12 -0300

    added cpp_the_programming_language_book

---
+++ cpp/cpp_the_programming_language_book/3_variable_types.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    bool v_bool = true;
+    cout << v_bool << "\n";
+
+    char v_char = 's';
+    cout << v_char << "\n";
+
+    int v_int = 123;
+    cout << v_int << "\n";
+
+    double v_double = 123.12;
+    cout << v_double << "\n";
+}
